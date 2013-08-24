bf9ff1a1961de2fab3a7d5012acc865498a7fe55
David Heryanto <david.heryanto@hotmail.com>
2013-08-24 18:51:39 +0800

    Add ignoreRemover and stringRotator class definition

---
+++ KwicKwakKwoc/KwicKwakKwoc/StringRotator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KwicKwakKwoc
+{
+    static class StringRotator
+    {
+        static List<string> PermuteWords(string line)
+        {
+            List<string> lines = new List<String>();
+            string[] words = line.Split(' ');
+
+
+
+            return null;
+        }
+    }
+}
